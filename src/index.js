412acc2cd7abb5c4e3c788ccc88af0e69ced487b
Fhillip Castillo <fcastillo@intellisys.com.do>
2018-02-28 15:45:02 -0400

    add home and move app styling to home

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './containers';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import{ Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(()=> {});
+const _root = (<Provider store={store}><App /></Provider>);
+
+ReactDOM.render(_root, document.getElementById('root'));
 registerServiceWorker();
